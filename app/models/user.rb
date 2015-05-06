246c354de8cac502235da5a1020304febcae633d
Nathaniel Clapp <github@nattyclapp.com>
2015-05-05 20:27:54 -0500

    fill out user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+
+  include BCrypt
+
+  has_many :entries
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    self.password_digest = Password.create(new_password)
+  end
+
 end
